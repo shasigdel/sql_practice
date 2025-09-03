68e94898a22466952ca4262e9c13be9554a0ee71
Shashank Sigdel <shasigdel@gmail.com>
2025-09-03 14:50:39 -0400

    first
    sql practice

---
+++ first.sql
@@ -0,0 +1,12 @@
+select * 
+from employee_demographics;
+
+select first_name,
+last_name,
+age,
+age + 10 as future 
+from employee_demographics;
+
+SELECT DISTINCT gender 
+FROM employee_demographics;
+
